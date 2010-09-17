8d5a26e0ded48d6be4535a4f65fdacb45d38cc22
Dave Perrett <dave@recurser.com>
2010-09-17 10:14:41 +1000

    Added static pages.

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module RecursiveRailsStarter
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.generators do |g|
+      g.template_engine :haml
+    end
+    
   end
 end
