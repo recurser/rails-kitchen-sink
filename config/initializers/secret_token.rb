0146d00b66297cce9ca805b4c5cd0be02f92a219
Dave Perrett <dave@recurser.com>
2010-09-17 10:03:49 +1000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecursiveRailsStarter::Application.config.secret_token = '6989ebc10c40766e02401e0021f1b225d66ac9f6d5f213f29ca9109bc463057347d79468351fc1a62eb1220926ac9838e45b38147a18fca0e73e54774cbcc5e6'
