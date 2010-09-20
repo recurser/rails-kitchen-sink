eb87561e48b860170bdec4f2c0d4a3f9caa36f8b
Dave Perrett <dave@recurser.com>
2010-09-20 20:36:18 +1000

    Heroku/compass test.

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -12,4 +12,4 @@ end
 
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
                                              :urls => ['/stylesheets'],
-                                             :root => "#{Rails.root}/tmp")
+                                             :root => "tmp")
