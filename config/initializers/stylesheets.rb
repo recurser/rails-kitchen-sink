8e347baf4895dd6a24857bc2c2076008e75593d1
Dave Perrett <dave@recurser.com>
2010-09-20 20:45:49 +1000

    Heroku/compass test.

--- config/initializers/stylesheets.rb
+++ config/initializers/stylesheets.rb
@@ -7,9 +7,9 @@ require 'fileutils'
 FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
 Sass::Plugin.on_updating_stylesheet do |template, css|
-  puts "Compiling #{template} to #{css}"
+  logger.info "Compiling #{template} to #{css}"
 end
 
 Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
                                              :urls => ['/stylesheets'],
-                                             :root => "tmp")
+                                             :root => "#{Rails.root}/tmp")
