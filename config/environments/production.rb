b8deae6b5b5992ff8eae7eb2593b30c04d8255db
Dave Perrett <dave@recurser.com>
2010-09-21 00:29:48 +1000

    Heroku/compass test.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ RecursiveRailsStarter::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # Don't try and compile SASS on Heroku
+  Sass::Plugin.options[:never_update] = true
 end
