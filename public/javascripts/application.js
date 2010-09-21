86605724037cdede0f58e585c500493b0b79e459
Dave Perrett <dave@recurser.com>
2010-09-21 23:58:23 +1000

    Updated jscript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,12 @@
 (function(){
-  var powers;
-  powers = [1, 2, 3, 4].map(function(i) {
-    return i * i;
-  });
-  alert(powers);
+  var app;
+  app = {
+    init: function() {
+      return app.show_messages();
+    },
+    show_messages: function() {
+      return $("#messages").slideDown('slow').fadeTo(3000, 1).slideUp('slow');
+    }
+  };
+  $(document).ready(app.init);
 })();
