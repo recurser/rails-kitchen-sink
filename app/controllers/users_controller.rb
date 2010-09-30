38cae25a5a8e19c8e2a771858e5f627d77bcd32f
Dave Perrett <dave@recurser.com>
2010-09-30 19:02:37 +1000

    Added basic user list page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,32 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   
   def index
+    @title = 'users.title_index'
+    @users = User.paginate(:page => params[:page])
   end
 
   def new
+    @title = 'users.title_new'
   end
 
   def create
+    @title = 'users.title_create'
   end
 
   def show
+    @title = 'users.title_show'
   end
 
   def edit
+    @title = 'users.title_edit'
   end
 
   def update
+    @title = 'users.title_update'
   end
 
   def destroy
+    @title = 'users.title_destroy'
   end
 
 end
