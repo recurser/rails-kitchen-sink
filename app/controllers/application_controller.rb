ebaab9afe7b55f6db09d52f6fa6abe216a7ff034
Dave Perrett <dave@recurser.com>
2010-09-30 19:18:46 +1000

    Added redirector for cancan exceptions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
+  
 end
