e739345f30ab8198ddd51ca9ec7bb06881554e51
Dave Perrett <dave@recurser.com>
2010-10-01 14:48:31 +1000

    Improved user management.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,7 @@ class UsersController < ApplicationController
   before_filter :accessible_roles, :only => [:new, :edit, :show, :update, :create]
   load_and_authorize_resource
  
-  # GET /users
-  # GET /users.xml                                                
-  # GET /users.json                                       HTML and AJAX
-  #-----------------------------------------------------------------------
+ 
   def index
     @title = 'users.title_index'
     @users = User.accessible_by(current_ability, :index).paginate(:page => params[:page])
@@ -16,79 +13,26 @@ class UsersController < ApplicationController
       format.html
     end
   end
- 
-  # GET /users/new
-  # GET /users/new.xml                                            
-  # GET /users/new.json                                    HTML AND AJAX
-  #-------------------------------------------------------------------
+  
+  
   def new
     @title = 'users.title_new'
+    @roles = Role.accessible_by(current_ability, :index)
     respond_to do |format|
       format.json { render :json => @user }   
       format.xml  { render :xml => @user }
       format.html
     end
   end
- 
-  # GET /users/1
-  # GET /users/1.xml                                                       
-  # GET /users/1.json                                     HTML AND AJAX
-  #-------------------------------------------------------------------
-  def show
-    @title = 'users.title_show'
-    respond_to do |format|
-      format.json { render :json => @user }
-      format.xml  { render :xml => @user }
-      format.html      
-    end
- 
-  rescue ActiveRecord::RecordNotFound
-    respond_to_not_found(:json, :xml, :html)
-  end
- 
-  # GET /users/1/edit                                                      
-  # GET /users/1/edit.xml                                                      
-  # GET /users/1/edit.json                                HTML AND AJAX
-  #-------------------------------------------------------------------
-  def edit
-    @title = 'users.title_edit'
-    respond_to do |format|
-      format.json { render :json => @user }   
-      format.xml  { render :xml => @user }
-      format.html
-    end
- 
-  rescue ActiveRecord::RecordNotFound
-    respond_to_not_found(:json, :xml, :html)
-  end
- 
-  # DELETE /users/1     
-  # DELETE /users/1.xml
-  # DELETE /users/1.json                                  HTML AND AJAX
-  #-------------------------------------------------------------------
-  def destroy
-    @title = 'users.title_destroy'
-    @user.destroy!
- 
-    respond_to do |format|
-      format.json { respond_to_destroy(:ajax) }
-      format.xml  { head :ok }
-      format.html { respond_to_destroy(:html) }      
-    end
- 
-  rescue ActiveRecord::RecordNotFound
-    respond_to_not_found(:json, :xml, :html)
-  end
- 
-  # POST /users
-  # POST /users.xml         
-  # POST /users.json                                      HTML AND AJAX
-  #-----------------------------------------------------------------
+  
+  
   def create
     @title = 'users.title_create'
     @user = User.new(params[:user])
  
     if @user.save
+      # Can't mass-assign role_ids - want to protect this from regular users.
+      @user.role_ids = params[:user][:role_ids]
       respond_to do |format|
         format.json { render :json => @user.to_json, :status => 200 }
         format.xml  { head :ok }
@@ -101,6 +45,77 @@ class UsersController < ApplicationController
         format.html { render :action => :new, :status => :unprocessable_entity }
       end
     end
+  end
+  
+  
+  def show
+    @title = 'users.title_show'
+    respond_to do |format|
+      format.json { render :json => @user }
+      format.xml  { render :xml => @user }
+      format.html      
+    end
+ 
+  rescue ActiveRecord::RecordNotFound
+    respond_to_not_found(:json, :xml, :html)
+  end
+  
+  
+  def edit
+    @title = 'users.title_edit'
+    @roles = Role.accessible_by(current_ability, :index)
+    respond_to do |format|
+      format.json { render :json => @user }   
+      format.xml  { render :xml => @user }
+      format.html
+    end
+ 
+  rescue ActiveRecord::RecordNotFound
+    respond_to_not_found(:json, :xml, :html)
+  end
+  
+  
+  def update
+    if params[:user][:password].blank?
+      [:password,:password_confirmation,:current_password].collect{|p| params[:user].delete(p) }
+    else
+      @user.errors[:base] << "The password you entered is incorrect" unless @user.valid_password?(params[:user][:current_password])
+    end
+ 
+    respond_to do |format|
+      
+      if @user.errors[:base].empty? and @user.update_attributes(params[:user])
+        # Can't mass-assign role_ids - want to protect this from regular users.
+        @user.role_ids = params[:user][:role_ids]
+      
+        flash[:notice] = "Your account has been updated"
+        format.json { render :json => @user.to_json, :status => 200 }
+        format.xml  { head :ok }
+        format.html { render :action => :edit }
+      else
+        format.json { render :text => "Could not update user", :status => :unprocessable_entity } #placeholder
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.html { render :action => :edit, :status => :unprocessable_entity }
+      end
+    end
+ 
+  rescue ActiveRecord::RecordNotFound
+    respond_to_not_found(:js, :xml, :html)
+  end
+  
+  
+  def destroy
+    @title = 'users.title_destroy'
+    @user.destroy
+ 
+    respond_to do |format|
+      format.json { redirect_to :action => :index }
+      format.xml  { head :ok }
+      format.html { redirect_to :action => :index }      
+    end
+ 
+  rescue ActiveRecord::RecordNotFound
+    respond_to_not_found(:json, :xml, :html)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :confirmed_at
-  
+    
   def role?(role)
     return !!self.roles.find_by_name(role.to_s.camelize)
   end
