2a20ae5e2da853dc37bdb0c75c63dd927fc443fc
Dave Perrett <dave@recurser.com>
2010-10-03 18:09:41 +1100

    Removed some unneccesary jscript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,14 +2,10 @@
   var app;
   app = {
     init: function() {
-      app.show_messages();
-      return app.hide_elements();
+      return app.show_messages();
     },
     show_messages: function() {
       return $("#messages").slideDown('slow').fadeTo(3000, 1).slideUp('slow');
-    },
-    hide_elements: function() {
-      return $("input#user_role_ids_2").parents('label').hide();
     }
   };
   $(document).ready(app.init);
