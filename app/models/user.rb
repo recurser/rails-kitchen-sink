15486b5f71b85551347a7af3b8710db3bd850229
Dave Perrett <dave@recurser.com>
2010-10-03 14:29:27 +1100

    Make sure all users have at least the :user role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,13 @@ class User < ActiveRecord::Base
     return !!Role.by_name(role)
   end
   
-  # Make sure everyone always has at least the :user role.
-  def role_ids=(val)
-    logger.debug '================================'
-    logger.debug val
+  # Make sure all users have at least the :user role.
+  def role_ids_with_add_user_role=(_role_ids) 
     user_role = Role.by_name(:user)
-    val << user_role.id unless val.include? user_role.id
-    @role_ids = val
-    logger.debug val
-    logger.debug '================================'
-    self.save
-  end
+    _role_ids << user_role.id
+    self[:role_ids] = _role_ids
+    self.role_ids_without_add_user_role = _role_ids
+   end 
+   alias_method_chain :role_ids=, :add_user_role 
   
 end
