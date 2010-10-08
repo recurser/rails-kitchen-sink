b6ce0b38360c351d0fe374952a6c85b2cc206a2d
Dave Perrett <dave@recurser.com>
2010-10-08 17:24:32 +1100

    Added role model tests.

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Role do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+  
+    it "should require a name" do
+      no_name_role = Role.new({:name => ''})
+      no_name_role.should_not be_valid
+    end
+  
+    it "name should be unique" do
+      role_1 = Role.new({:name => 'role_1'})
+      role_1.save
+      role_2 = Role.new({:name => 'role_1'})
+      role_2.should_not be_valid
+    end
+    
+  end
+
+  describe "convenience methods" do
+  
+    it "by_name should return the correct role" do
+      user_role = Role.new({:name => 'User'})
+      user_role.save
+      Role.by_name(:user).id.should == user_role.id
+    end
+    
+  end
+    
 end
