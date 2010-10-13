75b4b0010d12fce39460f1a8e2bc259d35e2de93
Dave Perrett <dave@recurser.com>
2010-10-13 21:25:39 +1100

    Tests for contact model.

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,3 +8,9 @@ Factory.define :user do |user|
   user.password               "123456"
   user.password_confirmation  "123456"
 end
+
+Factory.define :contact do |contact|
+  contact.email   "mail@recursive-design.com"
+  contact.subject "Urgent Problem"
+  contact.body    "Please reply ASAP!"
+end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Contact do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+  
+    it "should require an email" do
+      no_email_contact = Contact.new({:email => ''})
+      no_email_contact.should_not be_valid
+    end
+  
+    it "should require a valid email" do
+      invalid_email_contact = Contact.new({:email => 'test@test'})
+      invalid_email_contact.should_not be_valid
+    end
+  
+    it "should require a subject" do
+      no_subject_contact = Contact.new({:subject => ''})
+      no_subject_contact.should_not be_valid
+    end
+  
+    it "should require a body" do
+      no_body_contact = Contact.new({:body => ''})
+      no_body_contact.should_not be_valid
+    end
+    
+  end
+  
 end
