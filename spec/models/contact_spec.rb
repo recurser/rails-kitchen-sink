91296fcd4b45b27ddce8a1112cfc4c79eecc4ed3
Dave Perrett <dave@recurser.com>
2010-10-14 11:43:16 +1100

    Added contact model tests.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,28 +2,61 @@ require 'spec_helper'
 
 describe Contact do
 
-  describe "validations" do
+  before(:each) do
+    @attr = {
+      :email   => "mail@recursive-design.com",
+      :subject => "Urgent Problem",
+      :body    => "Please reply ASAP!"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    valid_contact = Contact.new(@attr)
+    valid_contact.should be_valid
+  end
+
+  it "should save successfully given valid attributes" do
+    valid_contact = Contact.new(@attr)
+    valid_contact.save.should == true
+  end
+
+  it "should fail to save given valid attributes" do
+    valid_contact = Contact.new(@attr.merge(:email => ""))
+    valid_contact.save.should == false
+  end
+
+  describe "email validations" do
   
-    it "should require an email" do
-      no_email_contact = Contact.new({:email => ''})
+    it "should require an email address" do
+      no_email_contact = Contact.new(@attr.merge(:email => ""))
       no_email_contact.should_not be_valid
     end
-  
-    it "should require a valid email" do
-      invalid_email_contact = Contact.new({:email => 'test@test'})
-      invalid_email_contact.should_not be_valid
+
+    it "should accept valid email addresses" do
+      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+      addresses.each do |address|
+        valid_email_contact = Contact.new(@attr.merge(:email => address))
+        valid_email_contact.should be_valid
+      end
     end
-  
-    it "should require a subject" do
-      no_subject_contact = Contact.new({:subject => ''})
-      no_subject_contact.should_not be_valid
+
+    it "should reject invalid email addresses" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |address|
+        invalid_email_contact = Contact.new(@attr.merge(:email => address))
+        invalid_email_contact.should_not be_valid
+      end
     end
-  
-    it "should require a body" do
-      no_body_contact = Contact.new({:body => ''})
-      no_body_contact.should_not be_valid
-    end
-    
+  end
+
+  it "should require a subject" do
+    no_subject_contact = Contact.new(@attr.merge(:subject => ""))
+    no_subject_contact.should_not be_valid
+  end
+
+  it "should require a body" do
+    no_body_contact = Contact.new(@attr.merge(:body => ""))
+    no_body_contact.should_not be_valid
   end
   
 end
